2ef423e5b4ad777cc30af0f1f78fd52c44edf86a
trueKrogoth <mars2d.rpg@gmail.com>
2015-07-20 17:43:47 +0300

    made a change

--- main/obj_constructor.hpp
+++ main/obj_constructor.hpp
@@ -33,7 +33,7 @@ template<class T, class C> TObject* __object_constructor(CObject* config, TObjec
 }
 
 #define DEFINE_OBJECT_CONSTRUCTOR(type_index, T, C) \
-__object_constructor_indexer type##_indexer_instance(type_index, &__object_constructor<T, C>); \
+__object_constructor_indexer T##_indexer_instance(type_index, &__object_constructor<T, C>); \
 T::T(int typeIndex, CObject* config, TObject* baseObject) : TObject(type_index, config, baseObject = 0)
 
 }
